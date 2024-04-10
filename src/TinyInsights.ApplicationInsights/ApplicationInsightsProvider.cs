4dd33600f4ea9c94b30bb62a21d197ad5dc0be9c
Daniel Hindrikes <daniel.hindrikes@cab.se>
2024-04-10 15:12:23 +0200

    cleaned up

--- src/TinyInsights.ApplicationInsights/ApplicationInsightsProvider.cs
+++ src/TinyInsights.ApplicationInsights/ApplicationInsightsProvider.cs
@@ -10,6 +10,8 @@ using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Devices;
 using Microsoft.Maui.Storage;
 using Newtonsoft.Json;
+using Microsoft.Maui;
+
 
 #if WINDOWS
 using Windows.Storage;
@@ -17,24 +19,24 @@ using Windows.UI.Xaml;
 #endif
 
 namespace TinyInsightsLib.ApplicationInsights;
-    public class ApplicationInsightsProvider : ITinyInsightsProvider
-    {
-        private const string userIdKey = nameof(userIdKey);
+public class ApplicationInsightsProvider : ITinyInsightsProvider
+{
+    private const string userIdKey = nameof(userIdKey);
 
-        private const string crashLogFilename = "crashes.tinyinsights";
+    private const string crashLogFilename = "crashes.tinyinsights";
+
+    private readonly string logPath = FileSystem.CacheDirectory;
+
+
+
+    private TelemetryClient client;
+
+    public bool IsTrackErrorsEnabled { get; set; } = true;
+    public bool IsTrackPageViewsEnabled { get; set; } = true;
+    public bool IsTrackEventsEnabled { get; set; } = true;
+    public bool IsTrackDependencyEnabled { get; set; } = true;
+
 #if IOS || MACCATALYST || ANDROID
-        private readonly string logPath = FileSystem.CacheDirectory;
-
-#endif
-
-        private TelemetryClient client;
-
-        public bool IsTrackErrorsEnabled { get; set; } = true;
-        public bool IsTrackPageViewsEnabled { get; set; } = true;
-        public bool IsTrackEventsEnabled { get; set; } = true;
-        public bool IsTrackDependencyEnabled { get; set; } = true;
-
-#if IOS|| MACCATALYST || ANDROID
         public ApplicationInsightsProvider(string connectionString)
         {
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
@@ -69,212 +71,208 @@ namespace TinyInsightsLib.ApplicationInsights;
         }
 
 #elif WINDOWS
-        public ApplicationInsightsProvider(Application app, string key)
-        {
-            app.UnhandledException += App_UnhandledException;
+    public ApplicationInsightsProvider(MauiWinUIApplication app, string connectionString)
+    {
+        app.UnhandledException += App_UnhandledException;
 
-            client = new TelemetryClient();
-            client.InstrumentationKey = key;
+        var configuration = new TelemetryConfiguration()
+        {
+            ConnectionString = connectionString
+        };
+
+        try
+        {
+            client = new TelemetryClient(configuration);
 
             AddMetaData();
-
-            Task.Run(SendCrashes);
         }
-
-        private void App_UnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+        catch (Exception)
         {
-            HandleCrash(e.Exception);
         }
+
+        AddMetaData();
+
+        Task.Run(SendCrashes);
+    }
+
+    private void App_UnhandledException(object sender, Microsoft.UI.Xaml.UnhandledExceptionEventArgs e)
+    {
+        HandleCrash(e.Exception);
+    }
 #endif
 
-        private void AddMetaData()
+    private void AddMetaData()
+    {
+        client.Context.Device.OperatingSystem = DeviceInfo.Platform.ToString();
+        client.Context.Device.Model = DeviceInfo.Model;
+        client.Context.Device.Type = DeviceInfo.Idiom.ToString();
+
+        //Role name will show device name if we don't set it to empty and we want it to be so anonymous as possible.
+        client.Context.Cloud.RoleName = string.Empty;
+        client.Context.Cloud.RoleInstance = string.Empty;
+
+        if (Preferences.ContainsKey(userIdKey))
         {
-            client.Context.Device.OperatingSystem = DeviceInfo.Platform.ToString();
-            client.Context.Device.Model = DeviceInfo.Model;
-            client.Context.Device.Type = DeviceInfo.Idiom.ToString();
+            var userId = Preferences.Get(userIdKey, string.Empty);
 
-            //Role name will show device name if we don't set it to empty and we want it to be so anonymous as possible.
-            client.Context.Cloud.RoleName = string.Empty;
-            client.Context.Cloud.RoleInstance = string.Empty;
+            client.Context.User.Id = userId;
+        }
+        else
+        {
+            var userId = Guid.NewGuid().ToString();
+            Preferences.Set(userIdKey, userId);
 
-            if (Preferences.ContainsKey(userIdKey))
-            {
-                var userId = Preferences.Get(userIdKey, string.Empty);
-
-                client.Context.User.Id = userId;
-            }
-            else
-            {
-                var userId = Guid.NewGuid().ToString();
-                Preferences.Set(userIdKey, userId);
-
-                client.Context.User.Id = userId;
-            }
-
-            client.Context.GlobalProperties.Add("Language", CultureInfo.CurrentUICulture.TwoLetterISOLanguageName);
-            client.Context.GlobalProperties.Add("Manufacturer", DeviceInfo.Manufacturer);
-            client.Context.GlobalProperties.Add("AppVersion", AppInfo.VersionString);
-            client.Context.GlobalProperties.Add("AppBuildNumber", AppInfo.BuildString);
+            client.Context.User.Id = userId;
         }
 
-        private async Task SendCrashes()
-        {
-            try
-            {
-                var crashes = ReadCrashes();
+        client.Context.GlobalProperties.TryAdd("Language", CultureInfo.CurrentUICulture.TwoLetterISOLanguageName);
+        client.Context.GlobalProperties.TryAdd("Manufacturer", DeviceInfo.Manufacturer);
+        client.Context.GlobalProperties.TryAdd("AppVersion", AppInfo.VersionString);
+        client.Context.GlobalProperties.TryAdd("AppBuildNumber", AppInfo.BuildString);
+    }
 
-                if (crashes != null)
+    private async Task SendCrashes()
+    {
+        try
+        {
+            var crashes = ReadCrashes();
+
+            if (crashes != null)
+            {
+                var properties = new Dictionary<string, string>
                 {
-                    var properties = new Dictionary<string, string>();
-                    properties.Add("IsCrash", "true");
-
-                    foreach (var crash in crashes)
-                    {
-                        await TrackErrorAsync(crash, properties);
-                    }
-                }
-            }
-            catch (Exception)
-            {
-            }
-        }
-
-        private List<Exception> ReadCrashes()
-        {
-            try
-            {
-#if IOS || MACCATALYST || ANDROID
-                var path = Path.Combine(logPath, crashLogFilename);
-
-                if (!File.Exists(path))
-                {
-                    return new List<Exception>();
-                }
-
-                var json = File.ReadAllText(path);
-#elif WINDOWS
-            
-            var fileTask = ApplicationData.Current.LocalCacheFolder.CreateFileAsync(crashLogFilename, CreationCollisionOption.OpenIfExists).AsTask<StorageFile>();
-            fileTask.Wait();
-            var file = fileTask.Result;
-
-            var readTask = FileIO.ReadTextAsync(file).AsTask<string>();
-            readTask.Wait();
-            var json = readTask.Result;
-#endif
-                if (string.IsNullOrWhiteSpace(json))
-                {
-                    return new List<Exception>();
-                }
-
-                return JsonConvert.DeserializeObject<List<Exception>>(json, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
-
-            }
-            catch (Exception)
-            {
-            }
-
-            return new List<Exception>();
-        }
-
-        private void HandleCrash(Exception ex)
-        {
-            try
-            {
-                var crashes = ReadCrashes();
-
-                crashes.Add(ex);
-
-                var json = JsonConvert.SerializeObject(crashes, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
-
-#if IOS|| MACCATALYST || ANDROID
-                var path = Path.Combine(logPath, crashLogFilename);
-
-                if (!File.Exists(path))
-                {
-                    File.Create(path);
-                }
-
-                System.IO.File.WriteAllText(path, json);
-#elif WINDOWS
-             var fileTask = ApplicationData.Current.LocalCacheFolder.CreateFileAsync(crashLogFilename, CreationCollisionOption.OpenIfExists).AsTask<StorageFile>();
-            fileTask.Wait();
-            var file = fileTask.Result;
-
-            var writeTask = FileIO.WriteTextAsync(file, json).AsTask();
-            writeTask.Wait();
-#endif
-            }
-            catch (Exception)
-            {
-            }
-        }
-
-        public async Task TrackErrorAsync(Exception ex, Dictionary<string, string> properties = null)
-        {
-            try
-            {
-                client.TrackException(ex, properties);
-                client.Flush();
-            }
-            catch (Exception)
-            {
-            }
-        }
-
-        public async Task TrackEventAsync(string eventName, Dictionary<string, string> properties = null)
-        {
-            try
-            {
-                client.TrackEvent(eventName, properties);
-                client.Flush();
-            }
-            catch (Exception ex)
-            {
-                _ = TinyInsights.TrackErrorAsync(ex);
-            }
-        }
-
-        public async Task TrackPageViewAsync(string viewName, Dictionary<string, string> properties = null)
-        {
-            try
-            {
-                client.TrackPageView(viewName);
-                client.Flush();
-            }
-            catch (Exception ex)
-            {
-                _ = TinyInsights.TrackErrorAsync(ex);
-            }
-        }
-
-        public async Task TrackDependencyAsync(string dependencyType, string dependencyName, DateTimeOffset startTime, TimeSpan duration, bool success, int resultCode = 0, Exception exception = null)
-        {
-            try
-            {
-                var dependency = new DependencyTelemetry(dependencyType, dependencyName, startTime, duration, success)
-                {
-                    ResultCode = resultCode.ToString()
+                    { "IsCrash", "true" }
                 };
 
-                if (exception != null)
+                foreach (var crash in crashes)
                 {
-                    var properties = new Dictionary<string, string>();
-                    properties.Add("Exception message", exception.Message);
-                    properties.Add("StackTrace", exception.StackTrace);
-
-                    if (exception.InnerException != null)
-                    {
-                        properties.Add("Inner exception message", exception.InnerException.Message);
-                        properties.Add("Inner exception stackTrace", exception.InnerException.StackTrace);
-                    }
+                    await TrackErrorAsync(crash, properties);
                 }
-
-                client.TrackDependency(dependency);
-            }
-            catch (Exception ex)
-            {
-                _ = TinyInsights.TrackErrorAsync(ex);
             }
         }
+        catch (Exception)
+        {
+        }
     }
+
+    private List<Exception> ReadCrashes()
+    {
+        try
+        {
+            var path = Path.Combine(logPath, crashLogFilename);
+
+            if (!File.Exists(path))
+            {
+                return new List<Exception>();
+            }
+
+            var json = File.ReadAllText(path);
+
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return new List<Exception>();
+            }
+
+            return JsonConvert.DeserializeObject<List<Exception>>(json, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+
+        }
+        catch (Exception)
+        {
+        }
+
+        return new List<Exception>();
+    }
+
+    private void HandleCrash(Exception ex)
+    {
+        try
+        {
+            var crashes = ReadCrashes();
+
+            crashes.Add(ex);
+
+            var json = JsonConvert.SerializeObject(crashes, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+
+            var path = Path.Combine(logPath, crashLogFilename);
+
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            System.IO.File.WriteAllText(path, json);
+
+        }
+        catch (Exception)
+        {
+        }
+    }
+
+    public async Task TrackErrorAsync(Exception ex, Dictionary<string, string> properties = null)
+    {
+        try
+        {
+            client.TrackException(ex, properties);
+            client.Flush();
+        }
+        catch (Exception)
+        {
+        }
+    }
+
+    public async Task TrackEventAsync(string eventName, Dictionary<string, string> properties = null)
+    {
+        try
+        {
+            client.TrackEvent(eventName, properties);
+            client.Flush();
+        }
+        catch (Exception ex)
+        {
+            _ = TinyInsights.TrackErrorAsync(ex);
+        }
+    }
+
+    public async Task TrackPageViewAsync(string viewName, Dictionary<string, string> properties = null)
+    {
+        try
+        {
+            client.TrackPageView(viewName);
+            client.Flush();
+        }
+        catch (Exception ex)
+        {
+            _ = TinyInsights.TrackErrorAsync(ex);
+        }
+    }
+
+    public async Task TrackDependencyAsync(string dependencyType, string dependencyName, DateTimeOffset startTime, TimeSpan duration, bool success, int resultCode = 0, Exception exception = null)
+    {
+        try
+        {
+            var dependency = new DependencyTelemetry(dependencyType, dependencyName, startTime, duration, success)
+            {
+                ResultCode = resultCode.ToString()
+            };
+
+            if (exception != null)
+            {
+                var properties = new Dictionary<string, string>();
+                properties.Add("Exception message", exception.Message);
+                properties.Add("StackTrace", exception.StackTrace);
+
+                if (exception.InnerException != null)
+                {
+                    properties.Add("Inner exception message", exception.InnerException.Message);
+                    properties.Add("Inner exception stackTrace", exception.InnerException.StackTrace);
+                }
+            }
+
+            client.TrackDependency(dependency);
+        }
+        catch (Exception ex)
+        {
+            _ = TinyInsights.TrackErrorAsync(ex);
+        }
+    }
+}
--- src/TinyTestApp/MauiProgram.cs
+++ src/TinyTestApp/MauiProgram.cs
@@ -21,8 +21,11 @@ public static class MauiProgram
         builder.Logging.AddDebug();
 #endif
 
+#if WINDOWS
+        TinyInsights.Configure(new ApplicationInsightsProvider(MauiWinUIApplication.Current, "InstrumentationKey=8b51208f-7926-4b7b-9867-16989206b950;IngestionEndpoint=https://swedencentral-0.in.applicationinsights.azure.com/;ApplicationId=0c04d3a0-9ee2-41a5-996e-526552dc730f"));
+#else
         TinyInsights.Configure(new ApplicationInsightsProvider("InstrumentationKey=8b51208f-7926-4b7b-9867-16989206b950;IngestionEndpoint=https://swedencentral-0.in.applicationinsights.azure.com/;ApplicationId=0c04d3a0-9ee2-41a5-996e-526552dc730f"));
-        
+#endif
         return builder.Build();
     }
 }
